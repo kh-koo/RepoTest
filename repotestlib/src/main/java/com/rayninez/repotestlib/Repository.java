6dcf308db7042d32d02fc006c3f28d90381bc2e1
Teo Koo <khkoo@buzzpia.com>
2015-03-12 18:27:35 +0900

    Initial import

---
+++ repotestlib/src/main/java/com/rayninez/repotestlib/Repository.java
@@ -0,0 +1,9 @@
+package com.rayninez.repotestlib;
+
+import android.util.Log;
+
+public class Repository {
+    public void HelloWorld() {
+        Log.d(Repository.class.getSimpleName(), "Hello World");
+    }
+}
